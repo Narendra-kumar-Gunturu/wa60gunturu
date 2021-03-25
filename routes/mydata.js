c50a40c8dc4bb5079fc986a9b53837eea6c9f7ef
Narendra-kumar-Gunturu <77824814+Narendra-kumar-Gunturu@users.noreply.github.com>
2021-03-24 21:54:43 -0500

    new endpoint

--- routes/mydata.js
+++ routes/mydata.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+/* GET home page. */
 router.get('/', function(req, res, next) {
-  res.send('User accesses are:'+data);
+  res.render('mydata', { title: 'Narendra Kumar Gunturu' });
 });
 
 module.exports = router;
